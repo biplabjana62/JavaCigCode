489217286b9d1b491e2b749d800338256c00ccda
biplabjana62 <biplabjana.09052005@gmail.com>
2026-01-22 10:05:21 +0530

    Improve string input and sorting in BubbleStr
    Changed prompts to use print for better input formatting and enabled case-insensitive sorting with compareToIgnoreCase. Also improved output formatting and cleaned up code style for better readability.

--- BubbleStr.java
+++ BubbleStr.java
@@ -3,32 +3,32 @@ import java.util.Scanner;
 public class BubbleStr {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.println("Enter a string size: ");
+        System.out.print("Enter a string size: ");
         int n = sc.nextInt(); // read the size of the string from the user
         sc.nextLine(); // <-- add this right after sc.nextInt() see explanation below
         String[] str = new String[n];
-        //System.out.println("Enter " + n + " strings:");
+        System.out.println("Enter " + n + " strings:");
         for (int i = 0; i < n; i++) {
             str[i] = sc.nextLine();
         }
         sc.close(); // close the scanner
-        System.out.println("Unsorted strings:");
+        System.out.println("\nUnsorted strings:");
         for (String s : str) {
             System.out.println(s);
         }
         // Bubble sort algorithm to sort the strings in ascending order
-        for (int i = 0; i < n-1; i++) {
+        for (int i = 0; i < n - 1; i++) {
             for (int j = 0; j < n - 1 - i; j++) {
-                if (str[j].compareTo(str[j + 1]) > 0) {
+                if (str[j].compareToIgnoreCase(str[j + 1]) > 0) {
                     String temp = str[j]; // swap the characters
                     str[j] = str[j + 1];
                     str[j + 1] = temp;
                 }
             }
         }
-        System.out.println("Sorted strings:");
-        for (int i =0; i < n; i++) {
-                    System.out.println(str[i]);
+        System.out.println("\nSorted strings:");
+        for (int i = 0; i < n; i++) {
+            System.out.println(str[i]);
         }
 
     }
