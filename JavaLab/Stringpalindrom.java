a24549cf52cfc5a86eac478bc1e27de3164951cc
biplabjana62 <biplabjana.09052005@gmail.com>
2026-02-01 20:36:17 +0530

    Update Stringpalindrom.java

--- JavaLab/Stringpalindrom.java
+++ JavaLab/Stringpalindrom.java
@@ -2,14 +2,14 @@ package JavaLab;
 
 import java.util.Scanner;
 // 8. Write a Java program that checks whether a given string is a palindrome or not. college lab
-public class Stringpalindrom {
+public class StringPalindrom {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter a string: ");
         String input = sc.nextLine();
         sc.close();
         StringBuilder sb = new StringBuilder(input);
-        Stringpalindrom sp = new Stringpalindrom();
+        StringPalindrom sp = new StringPalindrom();
         if (sp.isPalindrome(sb.toString())) {
             System.out.println(input + " is a palindrome.");
         } else {
