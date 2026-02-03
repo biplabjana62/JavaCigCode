49a7797049485ab8cbeb1eb6546e01d87323ae8e
biplabjana62 <biplabjana.09052005@gmail.com>
2026-02-03 11:56:58 +0530

    Update SingletonPattrn.java

--- SingletonPattrn.java
+++ SingletonPattrn.java
@@ -1,12 +1,12 @@
 class DemoSingleton {
-    private static DemoSingleton instance;
+    private static DemoSingleton instance; // static variable to hold single instance
 
     private DemoSingleton() {
     }
 
     public static DemoSingleton getInstanc() {
-        if (instance == null) {
-            instance = new DemoSingleton();
+        if (instance == null) { // check if instance is null
+            instance = new DemoSingleton(); // create new instance if null
         }
         return instance;
     }
@@ -14,6 +14,13 @@ class DemoSingleton {
 
 public class SingletonPattrn {
     public static void main(String[] args) {
-        
+        DemoSingleton obj1 = DemoSingleton.getInstanc();
+        DemoSingleton obj2 = DemoSingleton.getInstanc();
+        if (obj1 == obj2) {
+            System.out.println("Both objects are the same instance.");
+        } else {
+            System.out.println("Objects are different instances.");
+        }
+ 
     }
 }
